474268a1ba01bbb1f4ea90141f747c68904873b8
Ngari Ndung'u <ngari.ndungu@gmail.com>
2021-11-23 20:50:28 +0300

    script to fetch Steven Black's hosts file

---
+++ hosts/fetch_hosts_file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# adapted from; https://privacyinternational.org/guide-step/4340/linux-setup-dns-level-ad-blocking
+
+set -e
+
+curl -OL 'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/StevenBlack/hosts'
+
+if (grep -E '^(\s*)[1-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s*.*\..*' hosts); then
+  echo 'Potential malicious entry in hosts file. DO NOT USE!'
+  exit 1
+fi
+
+echo 'Concatenate downloaded file to your /etc/hosts to finish setup'
+# cat hosts >> /etc/hosts
